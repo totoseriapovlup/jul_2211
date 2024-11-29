8d2638af20d35c9e79efcbf2ec551e4d85e714a1
toto <test@test.com>
2024-11-29 19:54:48 +0200

    model methods for crud

--- app/models/Note.php
+++ app/models/Note.php
@@ -11,17 +11,22 @@ class Note
     public function __construct()
     {
         $this->db = new \mysqli(conf('DB_HOST'), conf('DB_USER'), conf('DB_PASS'), conf('DB_NAME'));
-        var_dump($this->db);
-        exit();
     }
 
-    public function all(){
-
+    public function all() : array
+    {
+        $query = "SELECT * FROM notes;";
+        $result = $this->db->query($query);
+        if($result){
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+        return [];
     }
 
-    public function add(array $note){
+    public function add(array $note) : bool
+    {
         $query = "INSERT INTO notes (name) VALUES ('{$note['name']}');";
-
+        return $this->db->query($query);
     }
 
     public function get($id){
@@ -32,8 +37,9 @@ class Note
 
     }
 
-    public function delete($id){
-
+    public function delete(int $id){
+        $query = "DELETE FROM notes WHERE id = $id;";
+        return $this->db->query($query);
     }
 
 }
